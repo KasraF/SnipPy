44fd48af17835e06cf4dbdbdecb1fc5bb70dd4c6
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-11-06 12:44:56 -0800

    Updated to latest commits

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ PYTHON3="$(which python3)"
 SCALA="$(which scala)"
 RUNPY="$(pwd)/vscode/src/run.py"
 IMGSUM="$(pwd)/vscode/src/img-summary.py"
-SYNTH="$(pwd)/synthesizer/target/scala-2.13/PythonSynthesizer-assembly-0.1.jar"
+SYNTH="$(pwd)/synthesizer/target/scala-2.13/SnippySynthesizer-assembly-0.1.jar"
 
 if [[ -d vscode ]]; then
     PYTHON3="$PYTHON3" SCALA="$SCALA" RUNPY="$RUNPY" IMGSUM="$IMGSUM" SYNTH="$SYNTH" ./vscode/scripts/code.sh;
