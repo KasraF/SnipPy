3d534473e43005a0b7daf1265654837973fe5a33
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2021-11-29 08:42:40 -0800

    allow using the run.sh script from any directory

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd vscode;
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR/vscode;
 SNIPPY_UTILS=src/snippy.py RUNPY=src/run.py IMGSUM=src/img-summary.py SYNTH=../synthesizer/target/snippy-server-0.1-SNAPSHOT-jar-with-dependencies.jar LOG_DIR=../logs PYTHON3=$(which python3) JAVA=$(which java) HEAP=20G ./scripts/code.sh
